85c1fcfe010fb0ac9189dc32360a0e9fdae8c93c
nmartoia <nmartoia@edenschool.fr>
2022-03-16 14:11:50 +0100

    mise en pause du 3eme test et commencement du 4eme test

--- js/app.js
+++ js/app.js
@@ -18,3 +18,26 @@ export const moyen = (entry) => {
     return tmp;
 
 }
+export const nostring = (entry) => {
+    let tmp=0
+
+    for (let i = 0; i < entry.length; i++) {
+        tmp+=entry[i]
+    }
+    tmp/=entry.length
+    return tmp;
+
+}
+export const pair = (entry) => {
+    let tmp=[[],[]]
+    for (let i = 0; i < entry.length; i++) {
+        if(entry[i]%2==1){
+            tmp[[0][0]].push(entry[i])
+        }
+        if(entry[i]%2==0){
+            tmp[[1][0]].push(entry[i])
+        }
+    }
+    return tmp;
+
+}
--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 import assert from 'assert';
 import { min } from '../js/app.js';
 import { moyen } from '../js/app.js';
+import { nostring } from '../js/app.js';
+import { pair } from '../js/app.js';
 describe('algotableau', function(){
   it('should return 0', function(){
     assert.equal(min([1,2,3,4,5,6,7,8,9,0,1000,29374]),0)
@@ -16,7 +18,34 @@ describe('algotableau', function(){
   it('should return 15', function(){
     assert.equal(moyen([10,20]),15)
   });
-  it('should return undifined', function(){
-    assert.equal(moyen([]),undefined)
+  it('should return NaN', function(){
+    assert.equal(moyen([]),NaN)
+  });
+});
+//describe('algotableau', function(){
+//  it('should return 15', function(){
+//    assert.equal(nostring("15"),15)
+//  });
+//  it('should return NaN', function(){
+//    assert.equal(nostring("salut"),NaN)
+//  });
+//  it('should return 6', function(){
+//    assert.equal(nostring(6), 6)
+//  });
+//});
+describe('algotableau', function(){
+  it('should return [[1,3,5],[2,4,6]]', function(){
+    assert.equal(pair([1,2,3,4,5,6]),[
+      [
+        1,
+        3,
+        5
+      ],
+      [
+        2,
+        4,
+        6
+      ]
+    ])
   });
 });
